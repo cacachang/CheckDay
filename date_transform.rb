460c26b0d431cd1c03679c6e552b2abbdcfdaa16
Ning <a24701770@gmail.com>
2024-04-04 21:29:06 +0800

    add: raise standerror when data type is not date

--- date_transform.rb
+++ date_transform.rb
@@ -1,15 +1,14 @@
 module DateTransform
   class Minguo
-    attr_reader :date
-
     def initialize(date:)
+      raise StandError('date must be Date type') unless date.kind_of?(Date)
       @date = date
     end
 
     def parse
-      year = date.year - 1911
-      month = sprintf('%02d', date.month) 
-      day = sprintf('%02d', date.day) 
+      year = @date.year - 1911
+      month = sprintf('%02d', @date.month) 
+      day = sprintf('%02d', @date.day) 
 
       minguo_year = year.to_i > 0 ? "民國 #{year}" : "民國前 #{(year + 1).abs}"
 
@@ -18,16 +17,12 @@ module DateTransform
   end
 
   class CommonEra
-    attr_reader :date
-
     def initialize(date:)
       @date = date
     end
 
     def parse
-      year = date.split("/")[0]
-      month = date.split("/")[1]
-      day = date.split("/")[2]
+      [year, month, day] = @date.split('/')
 
       common_era_year = year.to_i + 1911
 
